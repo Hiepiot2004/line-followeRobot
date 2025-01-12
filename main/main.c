dceb5b6cacef2e297e8a43623eb2e8d953aa6218
Hiep <tranhiep22102004@gmail.com>
2025-01-12 21:36:07 +0700

    khoi tao quang tro

--- main/main.c
+++ main/main.c
@@ -3,14 +3,31 @@
 #include "freertos/task.h"
 #include "driver/ledc.h"
 #include "pwm.h"
+#include "driver/adc.h"
+#include "esp_adc/adc_oneshot.h"
+#include "ldr.h"
 
 void app_main() {
     configure_pwm();
-    while (1) {
-        set_pwm_duty(MAX_DUTY / 4, MAX_DUTY / 4);  // Chạy cả hai động cơ ở tốc độ 50%
-        vTaskDelay(pdMS_TO_TICKS(2000));              // Chạy trong 2 giây
-        set_pwm_duty(MAX_DUTY/4, 0);      // Động cơ trái 100%, phải 25%
-        vTaskDelay(pdMS_TO_TICKS(1000));              // Chạy trong 2 giây
+    configure_adc();
 
+    while (1) {
+        int ldr_values[LDR_COUNT];
+        // Đọc giá trị từ các kênh của ADC1
+        for (int i = 0; i < 4; i++) {
+            adc_oneshot_read(adc1_handle, LDR_PINS[i], &ldr_values[i]);
+        }
+        // Đọc giá trị từ các kênh của ADC2
+        for (int i = 4; i < LDR_COUNT; i++) {
+            adc_oneshot_read(adc2_handle, LDR_PINS[i], &ldr_values[i]);
+        }
+
+        // Hiển thị giá trị lên monitor
+        for (int i = 0; i < LDR_COUNT; i++) {
+            printf("LDR value %d: %d\n", i, ldr_values[i]);
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(1000)); // Đọc giá trị mỗi giây
     }
+    
 }
